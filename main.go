f9b17d13f4138ddcb23839cba9706e69c4116a65
dellabeneta <m.dellabeneta@gmail.com>
2025-07-17 19:52:16 -0300

    ajuste

--- main.go
+++ main.go
@@ -75,7 +75,7 @@ const htmlTemplate = `
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gerador de CPF Brasileiro</title>
+    <title>Gerador de CPF</title>
 	<link rel="icon" href="/favicon.ico" type="image/x-icon">
 
     <style>
